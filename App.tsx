9ebed25ccbd4a7874e7f4a6bd02d272ff0b132be
GiseleBr <britto.gisele@gmail.com>
2022-11-16 17:04:46 -0300

    go

--- App.tsx
+++ App.tsx
@@ -1,26 +1,26 @@
-import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
+import { Text, View, TouchableOpacity, Image, ImageComponent } from "react-native";
 import { styles } from "./style";
-import Logo from "./assets/img/logo2.png";
 import { Ionicons } from "@expo/vector-icons";
+
+import Logo from "./assets/img/logo2.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
+      <View style={styles.container}>
+
       <View style={styles.buttonHeader}>
         <Image source={Logo} style={{ height: 70, width: 100 }} />
 
         <View style={styles.buttonContainer}>
           <Ionicons name="search-sharp" size={25} color="white" />
-            
+
           <Ionicons name="ios-person-outline" size={25} color="white" />
-          
+
           <TouchableOpacity style={styles.certificate}>
             <Text style={styles.textCertificate}>CERTIFICADOS</Text>
           </TouchableOpacity>
-        
         </View>
       </View>
-    </View>
+      </View>
   );
 }
--- style.ts
+++ style.ts
@@ -40,6 +40,9 @@ export const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
+    },
+
+    image: {
         
     }
 
